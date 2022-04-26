5c8dfab398a1af9bf3e0eccdea479aa722f23512
Igor <iigormazetti@hotmail.com>
2022-04-26 16:11:54 -0300

    tela de login, requisitos 02 ao 08

---
+++ src/pages/Login.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function Login() {
+  const history = useHistory();
+  const [btnDisabled, setButtonDisabled] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const verifyEmail = (em) => {
+    const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    return emailRegex.test(em);
+  };
+
+  useEffect(() => {
+    const handleVerification = () => {
+      const seis = 6;
+      if (verifyEmail(email) && password.length > seis) {
+        setButtonDisabled(false);
+      } else {
+        setButtonDisabled(true);
+      }
+    };
+    handleVerification();
+  }, [email, password]);
+
+  const handleButton = () => {
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/foods');
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form action="POST">
+        <input
+          type="text"
+          data-testid="email-input"
+          placeholder="Email"
+          value={ email }
+          onChange={ (e) => {
+            setEmail(e.target.value);
+          } }
+          name="email"
+          id="email"
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="Password"
+          value={ password }
+          onChange={ (e) => {
+            setPassword(e.target.value);
+          } }
+          name="password"
+          id="password"
+        />
+        <button
+          type="button"
+          onClick={ handleButton }
+          disabled={ btnDisabled }
+          data-testid="login-submit-btn"
+        >
+          Enter
+
+        </button>
+      </form>
+    </div>
+  );
+}

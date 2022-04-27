440160f1d6e109656458a09975f840de4a64312e
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-04-26 21:16:57 -0300

    Added item 82

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,67 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import BarraInferior from '../components/BarraInferior';
 
 export default function Profile() {
+  const history = useHistory();
+  const logOff = () => {
+    history.push('/');
+  };
+
+  const favoriteBtn = () => {
+    history.push('/favorite-recipes');
+  };
+
+  const doneBtn = () => {
+    history.push('/done-recipes');
+  };
+
   return (
     <div>
       <h2>Profile</h2>
+      {/* E-mail do usuário visível */}
+      <form action="POST">
+        <input
+          type="text"
+          data-testid="email-input"
+          placeholder="Email"
+          name="email"
+          id="email"
+        />
+
+        {/* Criação de 3 botões: Done, Recipe, Logout */}
+        <button
+          type="button"
+          data-testid="btn-done-recipe"
+          name="Done Recipes"
+          value="btn1"
+          placeholder="Done Recipes"
+          onClick={ doneBtn }
+        >
+          Receitas feitas
+        </button>
+        <button
+          type="button"
+          name="Favorite Recipes"
+          data-testid="btn-favorite-recipe"
+          value="btn2"
+          placeholder="Favorite Recipes"
+          onClick={ favoriteBtn }
+        >
+          Receitas favoritas
+        </button>
+        <button
+          type="button"
+          data-testid="btn-logout"
+          name="Logout"
+          value="btn3"
+          placeholder="Logout"
+          onClick={ logOff }
+        >
+          Logout
+        </button>
+      </form>
       <BarraInferior />
     </div>
-
   );
 }
